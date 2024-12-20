d32e481c8a53a8e0ce0a7f8db7fe7b449b8ad072
Vishnu P S <vishnu13three@gamil.com>
2024-12-20 09:53:41 +0530

    first commit

---
+++ src/controllers/employeeConytroller.ts
@@ -0,0 +1,9 @@
+class EmployeeController{
+    getAllEmployee = async(req:Express.Request,res:Express.Response){
+        try {
+            const employees = await
+        } catch (error) {
+            
+        }
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(express.json());
+
+const MONGO_URL = "mongodb://127.0.0.1:27017";
+mongoose.connect(MONGO_URL,{
+    dbName:"crud_application_typescript",
+}).then(()=>{
+    console.log("Database connected");
+}).catch((error)=>{
+    console.log(error);
+})
+
+app.listen(4000,()=>{
+    console.log(`Server running http://localhost:4000`);
+});
